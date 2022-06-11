c47e838aee7cb4c0fb4a5f3d96f5e8593dcfe01e
Lauren Leslie <laurenlesliesilva@gmail.com>
2022-06-11 20:54:05 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-   <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyles />
+    </>
   );
 }
 
