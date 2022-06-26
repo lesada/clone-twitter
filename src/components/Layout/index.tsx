8c4f2efdad1f9db7a86465ab09e9145875ab7563
Lauren Leslie <laurenlesliesilva@gmail.com>
2022-06-26 19:28:26 -0300

    Criação da barra de busca

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import  Main  from '../Main';
 import MenuBar from '../MenuBar';
+import SideBar from '../SideBar';
 
 import {Container, Wrapper} from './styles';
 
@@ -12,7 +13,7 @@ const Layout: React.FC = () => {
       <Wrapper>
         <MenuBar />
         <Main />
-        {/* <SideBar /> */}
+        <SideBar />
       </Wrapper>
     </Container>
   );
